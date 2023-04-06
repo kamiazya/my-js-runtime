6a9142daa5ac7062546d0ea9e9db4e1d00bcd925
kamiazya <yuki@kamiazya.tech>
2023-04-06 11:15:35 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,147 @@
+use deno_ast::MediaType;
+use deno_ast::ParseParams;
+use deno_ast::SourceTextInfo;
+use deno_core::OpState;
+use deno_core::error::AnyError;
+use deno_core::futures::FutureExt;
+use deno_core::op;
+use deno_core::JsRuntime;
+use deno_core::Extension;
+use std::rc::Rc;
+
+struct TsModuleLoader;
+
+impl deno_core::ModuleLoader for TsModuleLoader {
+    fn resolve(
+        &self,
+        specifier: &str,
+        referrer: &str,
+        _kind: deno_core::ResolutionKind,
+    ) -> Result<deno_core::ModuleSpecifier, deno_core::error::AnyError> {
+        deno_core::resolve_import(specifier, referrer).map_err(|e| e.into())
+    }
+
+    fn load(
+        &self,
+        module_specifier: &deno_core::ModuleSpecifier,
+        _maybe_referrer: Option<deno_core::ModuleSpecifier>,
+        _is_dyn_import: bool,
+    ) -> std::pin::Pin<Box<deno_core::ModuleSourceFuture>> {
+        let module_specifier = module_specifier.clone();
+        async move {
+            let path = module_specifier.to_file_path().unwrap();
+
+            // Determine what the MediaType is (this is done based on the file
+            // extension) and whether transpiling is required.
+            let media_type = MediaType::from(&path);
+            let (module_type, should_transpile) = match MediaType::from(&path) {
+                MediaType::JavaScript | MediaType::Mjs | MediaType::Cjs => {
+                    (deno_core::ModuleType::JavaScript, false)
+                }
+                MediaType::Jsx => (deno_core::ModuleType::JavaScript, true),
+                MediaType::TypeScript
+                | MediaType::Mts
+                | MediaType::Cts
+                | MediaType::Dts
+                | MediaType::Dmts
+                | MediaType::Dcts
+                | MediaType::Tsx => (deno_core::ModuleType::JavaScript, true),
+                MediaType::Json => (deno_core::ModuleType::Json, false),
+                _ => panic!("Unknown extension {:?}", path.extension()),
+            };
+
+            // Read the file, transpile if necessary.
+            let code = std::fs::read_to_string(&path)?;
+            let code = if should_transpile {
+                let parsed = deno_ast::parse_module(ParseParams {
+                    specifier: module_specifier.to_string(),
+                    text_info: SourceTextInfo::from_string(code),
+                    media_type,
+                    capture_tokens: false,
+                    scope_analysis: false,
+                    maybe_syntax: None,
+                })?;
+                parsed.transpile(&Default::default())?.text
+            } else {
+                code
+            };
+
+            // Load and return module.
+            let module = deno_core::ModuleSource {
+                code: code.into_bytes().into_boxed_slice(),
+                module_type,
+                module_url_specified: module_specifier.to_string(),
+                module_url_found: module_specifier.to_string(),
+            };
+            Ok(module)
+        }
+        .boxed_local()
+    }
+}
+
+#[op]
+async fn op_read_file(path: String) -> Result<String, AnyError> {
+    let contents = tokio::fs::read_to_string(path).await?;
+    Ok(contents)
+}
+
+#[op]
+async fn op_write_file(path: String, contents: String) -> Result<(), AnyError> {
+    tokio::fs::write(path, contents).await?;
+    Ok(())
+}
+
+#[op]
+fn op_remove_file(path: String) -> Result<(), AnyError> {
+    std::fs::remove_file(path)?;
+    Ok(())
+}
+
+#[op]
+async fn op_fetch(url: String) -> Result<String, AnyError> {
+    let body = reqwest::get(url).await?.text().await?;
+    Ok(body)
+}
+
+async fn run_js(file_path: &str) -> Result<(), AnyError> {
+    let main_module = deno_core::resolve_path(file_path)?;
+    let runjs_extension = Extension::builder("runjs")
+        .ops(vec![
+            op_read_file::decl(),
+            op_write_file::decl(),
+            op_remove_file::decl(),
+            op_fetch::decl(),
+        ])
+        .build();
+    let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
+        module_loader: Some(Rc::new(TsModuleLoader)),
+        extensions: vec![runjs_extension],
+        ..Default::default()
+    });
+    js_runtime
+        .execute_script("[runjs:runtime.js]", include_str!("./runtime.js"))
+        .unwrap();
+
+    let mod_id = js_runtime.load_main_module(&main_module, None).await?;
+    let result = js_runtime.mod_evaluate(mod_id);
+    js_runtime.run_event_loop(false).await?;
+
+    result.await?
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() <= 1 {
+        eprintln!("Usage: runjs <file>");
+        std::process::exit(1);
+    }
+    let file_path = &args[1];
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    if let Err(error) = runtime.block_on(run_js(file_path)) {
+        eprintln!("error: {}", error);
+    }
+}
---
+++ src/runtime.js
@@ -0,0 +1,37 @@
+// import { digraph } from 'npm:ts-graphviz';
+((globalThis) => {
+  const core = Deno.core;
+
+  // Note: Do not call this when snapshotting, it should be called
+  // at runtime. This example does not use V8 snapshots.
+  core.initializeAsyncOps();
+
+  function argsToMessage(...args) {
+    return args.map((arg) => JSON.stringify(arg)).join(" ");
+  }
+
+  globalThis.console = {
+    log: (...args) => {
+      core.print(`[out]: ${argsToMessage(...args)}\n`, false);
+    },
+    error: (...args) => {
+      core.print(`[err]: ${argsToMessage(...args)}\n`, true);
+    },
+  };
+
+  globalThis.runjs = {
+    readFile: (path) => {
+      return core.ops.op_read_file(path);
+    },
+    writeFile: (path, contents) => {
+      return core.ops.op_write_file(path, contents);
+    },
+    removeFile: (path) => {
+      return core.ops.op_remove_file(path);
+    },
+    fetch: (url) => {
+      return core.ops.op_fetch(url);
+    },
+  };
+
+})(globalThis);
